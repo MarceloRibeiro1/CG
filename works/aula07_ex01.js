de607f10dc2c1c5a5825c261ae36db3bcf932e6f
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-03-11 11:49:44 -0300

    Update aula07_ex01.js

--- works/aula07_ex01.js
+++ works/aula07_ex01.js
@@ -1,5 +1,6 @@
 import * as THREE from  '../build/three.module.js';
 import Stats from       '../build/jsm/libs/stats.module.js';
+import {GUI} from       '../build/jsm/libs/dat.gui.module.js';
 import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
 import {initRenderer, 
         initCamera,
@@ -18,14 +19,14 @@ var trackballControls = new TrackballControls( camera, renderer.domElement );
 
 // Show axes (parameter is size of each axis)
 var axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
+//scene.add( axesHelper );
 
 var groundPlane = createGroundPlane(10, 10); // width and height
   groundPlane.rotateX(degreesToRadians(-90));
   groundPlane.position.set(0,-0.01,0);
 scene.add(groundPlane);
 
-var light = initDefaultDirectionalLighting(scene, new THREE.Vector3(60, 60, 60));
+var light = initDefaultDirectionalLighting(scene, new THREE.Vector3(10, 10, 10));
 
 var textureloader = new THREE.TextureLoader();
 var textura = textureloader.load('../assets/textures/marble.png');
@@ -34,7 +35,10 @@ var planeGeometry = new THREE.PlaneGeometry(1.0, 1.0, 10, 10);
 var planeMaterial = new THREE.MeshLambertMaterial({color:"rgb(255,255,255)",side:THREE.DoubleSide});
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-//scene.add(plane);
+
+var alpha = 0.5;
+var planeMaterialAlpha = new THREE.MeshLambertMaterial({color:"red", opacity: alpha, transparent: true});
+
 
 var face1 = createFace(planeGeometry,planeMaterial,new THREE.Vector3(0,0,0),new THREE.Vector3(Math.PI/2,0,0),textura)
 var face2 = createFace(planeGeometry,planeMaterial,new THREE.Vector3(0,1,0),new THREE.Vector3(Math.PI/2,0,0),textura)
@@ -43,6 +47,8 @@ var face3 = createFace(planeGeometry,planeMaterial,new THREE.Vector3(0.5,0.5,0),
 var face4 = createFace(planeGeometry,planeMaterial,new THREE.Vector3(-0.5,0.5,0),new THREE.Vector3(0,Math.PI/2,0),textura)
 
 var face5 = createFace(planeGeometry,planeMaterial,new THREE.Vector3(0,0.5,-0.5),new THREE.Vector3(0,0,Math.PI/2),textura)
+var face6 = createFace2(planeGeometry,planeMaterialAlpha,new THREE.Vector3(0,0.5,0.5),new THREE.Vector3(0,0,Math.PI/2),textura)
+
 
 
 function createFace(geo,mat,pos,rot,tex){
@@ -54,7 +60,34 @@ function createFace(geo,mat,pos,rot,tex){
 
     return face;
 }
+function createFace2(geo,mat,pos,rot,tex){
+  var face = new THREE.Mesh(geo,mat)
+      face.position.set(pos.x,pos.y,pos.z);
+      face.rotation.set(rot.x,rot.y,rot.z);
+      //face.material.map = tex;
+  scene.add(face);
 
+  return face;
+}
+
+buildInterface();
+
+function buildInterface()
+{
+  var controls = new function ()
+  {
+    this.alpha = alpha;
+    this.changeAlpha = function(){
+      planeMaterialAlpha.opacity = this.alpha;
+    };
+  };
+
+  // GUI interface
+  var gui = new GUI();
+  gui.add(controls, 'alpha', 0.00, 1)
+    .onChange(function(e) { controls.changeAlpha() })
+    .name("Change Alpha");
+}
 
 // Listen window size changes
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
